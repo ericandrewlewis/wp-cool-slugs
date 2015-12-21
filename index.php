9065786f6ec7c08cc0a6edef9c16a1962f7198b7
Eric Lewis <eric.andrew.lewis@gmail.com>
2015-12-20 21:02:21 -0500

    Avoid collisions with custom post types.

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ Plugin Name: Cool Slugs
  * Assure post slug uniqueness, comparing the desired URL to existing rewrites.
  */
 add_filter( 'wp_unique_post_slug', function( $slug, $post_ID, $post_status, $post_type, $post_parent, $original_slug ) {
-  global $wp_rewrite;
+  global $wp, $wp_rewrite;
   $post = get_post( $post_ID );
   /*
    * The post may not exist yet, stub out a post object if so. This allows us to
@@ -28,12 +28,34 @@ add_filter( 'wp_unique_post_slug', function( $slug, $post_ID, $post_status, $pos
   $desired_permalink = get_permalink( $post );
   $parsed = parse_url( $desired_permalink );
   $desired_path = trim ( $parsed['path'], '/' );
-  $rewrite = $wp_rewrite->wp_rewrite_rules();
+
+  /*
+   * Copied from $wp->parse_request(). This function seems to do a lot of housekeeping
+   * which should probably be accessible if other things should be able to mock out and fake requests.
+   */
+  foreach ( get_post_types( array(), 'objects' ) as $post_type => $t ) {
+    if ( is_post_type_viewable( $t ) && $t->query_var ) {
+      $post_type_query_vars[$t->query_var] = $post_type;
+    }
+  }
+
   // Find a rewrite. Depends on #18877.
+  $rewrite = $wp_rewrite->wp_rewrite_rules();
   $found_rewrite_match = $wp_rewrite->find_match( $desired_path, $desired_path, $rewrite );
   if ( $found_rewrite_match ) {
     $query_string = $found_rewrite_match[1];
     parse_str( $query_string, $query_args );
+
+    foreach ( $wp->public_query_vars as $wpvar ) {
+			if ( !empty( $query_args[$wpvar] ) ) {
+				if ( isset($post_type_query_vars[$wpvar] ) ) {
+					$query_args['post_type'] = $post_type_query_vars[$wpvar];
+					$query_args['name'] = $query_args[$wpvar];
+          // Unset the `{post_type_name}` query variable, as the `name` qv will suffice.
+          unset( $query_args[$wpvar] );
+				}
+			}
+		}
     // Don't check for uniqueness against self.
     $query_args['post__not_in'] = array( $post_ID );
     $query_args['post_status'] = 'any';
